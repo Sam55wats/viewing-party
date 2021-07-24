9ad2e213e7cb8cf7a9b511e1b1abead212301087
Samuel Kim <sam55wats@gmail.com>
2021-07-23 22:37:38 -0400

    added a watch_movie function

--- viewing_party/main.py
+++ viewing_party/main.py
@@ -10,12 +10,18 @@ def create_movie(title, genre, rating):
     return None
 
 def add_to_watched(user_data, movie):
-    watched.append(movie)
+    user_data['watched'].append(movie)
     return user_data
 
-def add_to_watchlist(user_data, movie)
-    watchlist.append(movie)
-    return user_data
+def add_to_watchlist(user_data, movie):
+    user_data['watchlist'].append(movie)
+    return user_data 
 
-def watch_movie(user_data, title)
+def watch_movie(user_data, title):
+    if title in user_data['watchlist']:
+        user_data['watchlist'].remove(movie)
+        user_data['watched'].append(movie)
+        return user_data
+    
+    return user_data
     
